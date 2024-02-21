178d638de80e26e6ef7c07e4dcba0dcf709d8094
Дмитрий Абабков <dmitry@MacBook-Air-Dmitrij.local>
2024-02-21 23:35:34 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    entry: './src/js/index.js', // Точка входа для сборки проекта
+
+    output: {
+        filename: 'bundle.js', // Имя выходного файла сборки
+        path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        })
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'), // Каталог для статики
+        },
+        open: true, // Автоматически открывать браузер
+    },
+
+    mode: 'development', // Режим сборки
+};
