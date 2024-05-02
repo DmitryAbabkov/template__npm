8d4f1c51b3ea10357f8218a5b2cdf161085a5ba5
DmitryAbabkov <125596061+DmitryAbabkov@users.noreply.github.com>
2024-05-03 01:01:07 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            inject: false,
         }),
         new MiniCssExtractPlugin({
             filename: "bundle.css",
